0ab1c4bfd48b65b2a00f133a45d2a400a7fb144e
Lucia Segovia De La Revilla <lshls9@lshtm.ac.uk>
2023-07-25 11:35:45 +0100

    updated script according to uon_fao repo

--- WA19/WA19_FCT_FAO_Tags.R
+++ WA19/WA19_FCT_FAO_Tags.R
@@ -39,21 +39,21 @@ fgwa <- wafct %>% filter(is.na(food_desc), !is.na(fdc_id)) %>% pull(fdc_id) %>%
 
 wafct <- wafct %>% #Identifies the food group number from the fdc_id, and applies the correct food_group from the fgwa list to the food_group column
   mutate(food_group =
-           ifelse(grepl("01_", fdc_id), fgwa[1],
-                  ifelse(grepl("02_", fdc_id), fgwa[2],
-                         ifelse(grepl("03_", fdc_id), fgwa[3], 
-                                ifelse(grepl("04_", fdc_id), fgwa[4], 
-                                       ifelse(grepl("05_", fdc_id), fgwa[5],
-                                              ifelse(grepl("06_", fdc_id), fgwa[6],
-                                                     ifelse(grepl("07_", fdc_id), fgwa[7],
-                                                            ifelse(grepl("08_", fdc_id), fgwa[8],
-                                                                   ifelse(grepl("09_", fdc_id), fgwa[9], 
-                                                                          ifelse(grepl("10_", fdc_id), fgwa[10], 
-                                                                                 ifelse(grepl("11_", fdc_id), fgwa[11],
-                                                                                        ifelse(grepl("12_", fdc_id), fgwa[12],     
-                                                                                               ifelse(grepl("13_", fdc_id), fgwa[13],
-                                                                                                      ifelse(grepl("14_", fdc_id), fgwa[14],
-                                                                                                             'NA'))))))))))))))) %>% 
+  ifelse(grepl("01_", fdc_id), fgwa[1],
+  ifelse(grepl("02_", fdc_id), fgwa[2],
+  ifelse(grepl("03_", fdc_id), fgwa[3], 
+  ifelse(grepl("04_", fdc_id), fgwa[4], 
+  ifelse(grepl("05_", fdc_id), fgwa[5],
+  ifelse(grepl("06_", fdc_id), fgwa[6],
+  ifelse(grepl("07_", fdc_id), fgwa[7],
+  ifelse(grepl("08_", fdc_id), fgwa[8],
+  ifelse(grepl("09_", fdc_id), fgwa[9], 
+  ifelse(grepl("10_", fdc_id), fgwa[10], 
+  ifelse(grepl("11_", fdc_id), fgwa[11],
+  ifelse(grepl("12_", fdc_id), fgwa[12],     
+  ifelse(grepl("13_", fdc_id), fgwa[13],
+  ifelse(grepl("14_", fdc_id), fgwa[14],
+  'NA'))))))))))))))) %>% 
   filter(!is.na(food_desc)) #Removes any rows without a food description entry (the food group name rows, and a row that have already been used for naming)
 
 wafct <- wafct %>% slice(-1) #Removes the first row (which was used for the automatic renaming, and is now not useful)
@@ -77,17 +77,13 @@ wa_meta_quality <- wafct %>% mutate_at(wa_nut,  ~case_when(
 #and reported as using [] and removing them from the original variable
 
 wafct <- wafct %>% 
-  mutate(FATCEg = str_extract(FATg, '(?<=\\[).*?(?=\\])')) %>% #Creating calculated values from the lower quality method and removing the original values from the original variable
-  mutate(FIBCg = str_extract(FIBTGg, '(?<=\\[).*?(?=\\])')) %>% #e.g. this creates the FIBCg value from the FIBTGg value
-  mutate(CARTBmcg = str_extract(CARTBEQmcg, '(?<=\\[).*?(?=\\])')) %>%
-  mutate(TOCPHAmg = str_extract(VITEmg, '(?<=\\[).*?(?=\\])')) %>%
-  mutate(NIAmg = str_extract(NIAEQmg, '(?<=\\[).*?(?=\\])')) %>%
-  mutate(FOLSUMmcg = str_extract(FOLmcg, '(?<=\\[).*?(?=\\])')) %>%
-  mutate(PHYTCPPD_PHYTCPPImg = str_extract(PHYTCPPmg, '(?<=\\[).*?(?=\\])')) %>% 
-  mutate_at(c("FATg", "FIBTGg", "CARTBEQmcg", 
-              "VITEmg", "NIAEQmg", "FOLmcg", "PHYTCPPmg"),
-            ~ifelse(str_detect(. , '\\[.*?\\]') == TRUE, NA, #This removes the square brackets entries from the original column
-                    .))
+  mutate(FATCEg = str_extract(FATg, '(?<=\\[).*?(?=\\])'),  #Creating calculated values from the lower quality method and removing the original values from the original variable
+         FIBCg =  str_extract(FIBTGg, '(?<=\\[).*?(?=\\])'), #e.g. this creates the FIBCg value from the FIBTGg value
+         CARTBmcg = ifelse(is.na(CARTBmcg), str_extract(CARTBEQmcg, '(?<=\\[).*?(?=\\])'), CARTBmcg), 
+         TOCPHAmg = ifelse(is.na(TOCPHAmg),str_extract(VITEmg, '(?<=\\[).*?(?=\\])'), TOCPHAmg ),
+         NIAmg = ifelse(is.na(NIAmg), str_extract(NIAEQmg, '(?<=\\[).*?(?=\\])'), NIAmg), 
+         FOLSUMmcg = str_extract(FOLmcg, '(?<=\\[).*?(?=\\])'), 
+         PHYTCPPD_PHYTCPPImg = str_extract(PHYTCPPmg, '(?<=\\[).*?(?=\\])'))
 
 
 #The following f(x) removes [] and changing tr w/ 0
