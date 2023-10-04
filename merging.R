b347a1f48652c4c503131a177fad2b17783d12ce
Lucia Segovia De La Revilla <lshls9@lshtm.ac.uk>
2023-10-04 18:48:28 +0100

    loading-script

--- merging.R
+++ merging.R
@@ -12,32 +12,9 @@ library(measurements) # For unit conversion
 source(here::here("functions.R")) # Loading nutrition functions (change to package when ready)
 library(visdat) # Data visualisation
 
-#0) Only run if first time or updated original FCDB scripts ----
-#There are four scripts that need to run from the file
-#AU19/AU19_FCT_FAO_Tags.R
-#JA15/JA15_FCT_FAO_Tags.R
-#US19/US19_FCT_FAO_Tags.R
-#BR11/BR11_FCT_FAO_Tags.R
-
-#To run, remove the # and run
-
-##Checking and loading updates
-
-#source_fct_name <- c("DK19" ,
-#                     "IN17" ,
-#                     "KE18" ,   
-#                     "NZ18" ,
-#
-#                     "BA13" ,  
-#                     "UF16" ,
-#                     "WA19" 
-#                      )
-#
-#
-#for(i in source_fct_name){
-#  source(paste0(i, "/", i, "_FCT_FAO_Tags.R"))
-#}
-
+# 0) Only run if first time or updated original FCDB scripts ----
+# There are four scripts that need to run from the file
+# source(here::here("harmonisation", "load-fct.R"))
 
 # 1) Loading all FCDBs into one single database ----
 # This is possible because all the FCTs have been standardised previously.
@@ -45,7 +22,8 @@ library(visdat) # Data visualisation
 # finding all the cleaned FCTs/FCDBs from the output folder
 list.files("Output/", pattern = "*_FCT_FAO_Tags", recursive=FALSE, # so it is not taking the fcts in the folder
            full.names=TRUE) %>% 
-  map_df(~read_csv(., col_types = cols(.default = "c"), locale = locale(encoding = "Latin1")))  
+  map_df(~read_csv(., col_types = cols(.default = "c"), 
+                   locale = locale(encoding = "Latin1")))  
 
 # Importing all the cleaned FCTs/FCDBs into one single object (data.frame)
 fct_cover <- list.files("Output/", pattern = "*_FCT_FAO_Tags", recursive=FALSE, full.names=TRUE) %>% 
@@ -58,7 +36,8 @@ ics_code_file <- readRDS(here::here("data", "ics-code_fish-code.RDS"))
 
 # Checking that we have loaded all the FCT/FCDBs (n=11, excluding UK21 & NO21)
 fct_cover %>% distinct(source_fct) 
-names(ics_code_file )
+
+names(ics_code_file)
 length(unique(ics_code_file$ICS.FAOSTAT.SUA.Current.Code))
 dim(ics_code_file)
 
@@ -80,10 +59,13 @@ fish_fct <- fct_cover %>%
 
 dim(fish_fct)
 
-#Checking the no. of entries after filtering out all foods but fish
+# Checking the no. of entries after filtering out all foods but fish
 table(fish_fct$source_fct)
 table(fish_fct$ICS.FAOSTAT.SUA.Current.Code)
 length(unique(fish_fct$ICS.FAOSTAT.SUA.Current.Code))
 table(fish_fct$ICS.FAOSTAT.SUA.Current.Code, fish_fct$source_fct)
 
-fish_fct %>% group_by(ICS.FAOSTAT.SUA.Current.Code) %>% count() %>% pull() %>% max()
+fish_fct %>% group_by(ICS.FAOSTAT.SUA.Current.Code) %>%
+  count() %>% pull() %>% max()
+
+table(!is.na(fish_fct$scientific_name), fish_fct$source_fct)
